917b3c804449d6fd534b5d0dbf91bc65f77e9ca0
Alexis Dupire <adupire4@gmail.com>
2025-05-07 16:16:05 +0200

    Lobby added

---
+++ Source/Lobby/InitiFile.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LOBBY_API InitiFile
+{
+public:
+	InitiFile();
+	~InitiFile();
+};
---
+++ Source/Lobby/PlayerLobbyInfo.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "UObject/Object.h"
+#include "PlayerLobbyInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPlayerLobbyInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LobbyData")
+	APlayerController* PC;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LobbyData")
+	FText PlayerName;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LobbyData")
+	bool bReady = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LobbyData")
+	APlayerState* PlayerState;
+	
+};
